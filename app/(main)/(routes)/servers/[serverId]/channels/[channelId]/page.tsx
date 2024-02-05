bda5f95c2034f1c5a8c67e576b2f673085ebe3bb
Sahil <dshiv867@gmail.com>
2024-02-05 06:29:50 +0530

    mobile toggle

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,43 @@
+import { ChatHeader } from "@/components/chat/ChatHeader";
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { Divide } from "lucide-react";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+interface ChannelIdPageProps {
+  params: { serverId: string; channelId: string };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
